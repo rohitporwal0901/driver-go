ccd2acfb2191e4e1c23973236897d647ea9c659f
rohitQpl <rohit@quadralyst.com>
2026-06-10 18:54:54 +0530

    chanegs for mobile map isuse

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -36,7 +36,7 @@ export class MapService {
       attributionControl: false,
     });
 
-    L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
       maxZoom: 20,
       subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
     }).addTo(map);
